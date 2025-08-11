f7819ca496508914699f1bc08851d9dd7e798ec7
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-11 15:03:12 -0300

    add repositories

---
+++ src/main/java/com/rockin/api/repositories/CouponRepository.java
@@ -0,0 +1,10 @@
+package com.rockin.api.repositories;
+
+import com.rockin.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+}
---
+++ src/main/java/com/rockin/api/repositories/EventRepository.java
@@ -0,0 +1,10 @@
+package com.rockin.api.repositories;
+
+import com.rockin.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+
+}
