31160bc7195e2b98316ea971155c7fe2ddec962b
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-26 19:54:39 -0300

    Updated Address.

--- src/main/java/com/rockin/api/domain/address/Address.java
+++ src/main/java/com/rockin/api/domain/address/Address.java
@@ -2,11 +2,19 @@ package com.rockin.api.domain.address;
 
 import com.rockin.api.domain.event.Event;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Table(name = "address")
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     @Id
