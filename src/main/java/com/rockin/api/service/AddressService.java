3cb1ec297e11b93ef48733f52a9fcd49275b8a49
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-26 19:54:29 -0300

    Added AddressService.

---
+++ src/main/java/com/rockin/api/service/AddressService.java
@@ -0,0 +1,26 @@
+package com.rockin.api.service;
+
+import com.rockin.api.domain.address.Address;
+import com.rockin.api.domain.event.Event;
+import com.rockin.api.domain.event.EventRequestDTO;
+import com.rockin.api.repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    public AddressRepository addressRepository;
+
+    public Address createAddress(EventRequestDTO data, Event event) {
+        Address address = new Address();
+        address.setCity(data.city());
+        address.setUf(data.state());
+        address.setEvent(event);
+
+        return addressRepository.save(address);
+    }
+
+
+}
