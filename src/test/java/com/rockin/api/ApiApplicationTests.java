e9678e0d4de6dfdff68b24aae6e36dd1f97a0d90
PassamaniArthur <passamaniarthur@gmail.com>
2025-09-02 15:55:34 -0300

    Updated ApiApplicationTests

--- src/test/java/com/rockin/api/ApiApplicationTests.java
+++ src/test/java/com/rockin/api/ApiApplicationTests.java
@@ -2,8 +2,11 @@ package com.rockin.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
 
 	@Test
